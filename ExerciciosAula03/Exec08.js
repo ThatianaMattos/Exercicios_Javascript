67fd9f676d198bed6f5e112ee048d747cfb03746
ThatianaMattos <thatianamattos25@gmail.com>
2025-11-04 21:06:00 -0300

    feat: continuação dos exercícios Javascript aula 03

--- ExerciciosAula03/Exec08.js
+++ ExerciciosAula03/Exec08.js
@@ -1,33 +1,33 @@
-const leia = require('readline-sync');
+const leia = require("readline-sync");
 
 let vetor = [];
 let soma = 0;
 
 // Entrada de dados
 for (let i = 0; i < 10; i++) {
-    let numero = leia.questionInt(`Digite o ${i+1}º numero: `);
-    vetor[i] = numero;
-    soma += vetor[i];
+  let numero = leia.questionInt(`Digite o ${i + 1}º numero: `);
+  vetor[i] = numero;
+  soma += vetor[i];
 }
 
 // Elementos nos índices ímpares
 console.log("\nElementos nos índices ímpares:");
 for (let i = 1; i < vetor.length; i += 2) {
-    console.log(vetor[i]);
+  console.log(vetor[i]);
 }
 
 // Elementos pares
 console.log("\nElementos pares:");
 for (let i = 0; i < vetor.length; i++) {
-    if (vetor[i] % 2 === 0) {
-        console.log(vetor[i]);
-    }
+  if (vetor[i] % 2 === 0) {
+    console.log(vetor[i]);
+  }
 }
 
 // Soma e média
 
 for (let i = 0; i < vetor.length; i++) {
-    soma += vetor[i];
+  soma += vetor[i];
 }
 
 let media = soma / vetor.length;
